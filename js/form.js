08821e8dec466309745feac9025dd2d41e9cb3aa
blazhko22 <86318145+blazhko22@users.noreply.github.com>
2022-11-09 21:31:40 +0200

    2

--- js/form.js
+++ js/form.js
@@ -11,9 +11,10 @@ function handleSubmit(event) {
   if (username.value === "" || mail.value === "" || phone_number.value === "" || feedback.value === "") {
     return alert("Please fill in all the fields!");
   } else {
+    console.log(`Name: ${username.value}, Mail: ${mail.value}, Phone: ${phone_number.value}, Message: ${feedback.value}`);
+    event.currentTarget.reset();
     return alert("Thank you, your application is registered");
   }
 
-//   console.log(`Name: ${username.value}, Mail: ${mail.value}, Phone: ${phone_number.value}, Message: ${feedback.value}`);
-//   event.currentTarget.reset();
+  
 }
